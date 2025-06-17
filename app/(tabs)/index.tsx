9f6525099ba9b0222c301259ed661e2f970073f8
unknown <leonardo.vinicius@preve.g12.br>
2025-06-17 16:49:45 -0300

    hj

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,11 +3,16 @@ import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+
 
 const PlaceholderImage = require('@/assets/images/oi.png');
 
 export default function Index() {
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+    const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+
 
     const pickImageAsync = async () =>{
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -17,20 +22,43 @@ export default function Index() {
     });
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('Você não selecionou nenhuma imagem.');
     }
   };
+    const onReset = () => {
+  setShowAppOptions(false);
+};
+
+const onAddSticker = () => {
+};
+
+const onSaveImageAsync = async () => {
+
+};
+
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>      
       <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
+      {showAppOptions ? (
+  <View style={styles.optionsContainer}>
+    <View style={styles.optionsRow}>
+      <IconButton icon="refresh" label="Recarregar" onPress={onReset} />
+      <CircleButton onPress={onAddSticker} />
+      <IconButton icon="save-alt" label="Salvar" onPress={onSaveImageAsync} />
+    </View>
+  </View>
+    ) : (
+
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Escolha uma foto" onPress={pickImageAsync}/>
-        <Button label="Use esta foto" />
+        <Button label="Use esta foto" onPress={() => setShowAppOptions(true)} />
       </View>
+      )}
     </View>
   );
 }
@@ -49,4 +77,12 @@ const styles= StyleSheet.create({
       flex: 1 / 3,
       alignItems: 'center',
     },
+    optionsContainer: {
+  position: 'absolute',
+  bottom: 80,
+},
+optionsRow: {
+  alignItems: 'center',
+  flexDirection: 'row',
+},
 });
